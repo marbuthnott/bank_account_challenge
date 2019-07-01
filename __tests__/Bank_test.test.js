16224efa9ca1accbe249362ee69f7c42e9357757
Student <magnusarbuthnott@gmail.com>
2019-07-01 14:47:23 +0100

    test RED. Wrote first test for bank object on #deposit.

--- __tests__/Bank_test.test.js
+++ __tests__/Bank_test.test.js
@@ -3,9 +3,22 @@ import Bank from 'lib/Bank'
 describe('Bank', function() {
   var bank;
 
+  beforeEach(function() {
+    bank = new Bank();
+  });
+
   describe('#print_bank_statement', function() {
     it('prints a bank statement in the correct format', function() {
 
+    });
+  });
+
+  describe('#deposit', function() {
+    it('accepts a deposit', function() {
+      bank.deposit(2000)
+      var today = new Date();
+      var date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
+      expect(bank._account).toEqual([{date: date, credit: 2000, debit: 0, balance: 2000}])
     });
   });
 })
